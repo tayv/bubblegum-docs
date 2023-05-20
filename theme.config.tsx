670dd087099b80c862c55860e444836a6eeae20e
tayv <48400779+tayv@users.noreply.github.com>
2023-05-20 12:07:22 -0600

    Added open graph and seo to config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,58 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useConfig, DocsThemeConfig } from 'nextra-theme-docs'
 
-const config: DocsThemeConfig = {
-  head: (
-    <>
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="Bubblegum UI" />
-      <meta property="og:description" content="Documentation for React UI and Controlled Form Components" />
-    </>
-  ),
+const OG_IMAGE_URL = "/bubblegum_logo.svg"
+export default {
+  // head: (
+  //   <>
+  //     <link rel="shortcut icon" href="/favicon.ico" />
+  //     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  //     <meta property="og:title" content="Bubblegum UI" />
+  //     <meta property="og:description" content="Documentation for React UI and Controlled Form Components" />
+  //   </>
+  // ),
+  head() {
+    const { frontMatter } = useConfig();
+
+    return (
+      <>
+        {/* Favicons, meta */}
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta
+          name="description"
+          content={
+            frontMatter.description ||
+            "Controlled React Form + UI Components built with TailwindCSS, Radix UI, and React Hook Form."
+          }
+        />
+        <meta
+          name="og:description"
+          content={
+            frontMatter.description ||
+            "Controlled React Form + UI Components built with TailwindCSS, Radix UI, and React Hook Form."
+          }
+        />
+        {/* <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@handlehere" />
+        <meta name="twitter:image" content={OG_IMAGE_URL} /> */}
+        <meta
+          name="og:title"
+          content={
+            frontMatter.title
+              ? frontMatter.title + " – Bubblegum UI"
+              : "Bubblegum Documentation"
+          }
+        />
+        <meta name="og:image" content={OG_IMAGE_URL} />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </>
+    );
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – Bubblegum UI",
+    };
+  },
   logo: (
     <>
       <img src="/bubblegum_logo.svg" alt="Bubblegum Logo" style={{ width: '34px', height: '34px' }}/>
@@ -36,4 +79,4 @@ const config: DocsThemeConfig = {
   },
 }
 
-export default config
+
